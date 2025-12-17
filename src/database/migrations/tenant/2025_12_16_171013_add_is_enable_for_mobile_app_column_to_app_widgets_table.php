3671c397e5314472715cf203bbd1a75cea139b44
chamodrandeni <chamoddushantha2014@gmail.com>
2025-12-17 13:31:25 +0530

    ..

---
+++ src/database/migrations/tenant/2025_12_16_171013_add_is_enable_for_mobile_app_column_to_app_widgets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('app_widgets', function (Blueprint $table) {
+            $table->boolean('is_enable_for_web_app')->default(true);
+            $table->boolean('is_enable_for_mobile_app')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('app_widgets', function (Blueprint $table) {
+            $table->dropColumn('is_enable_for_web_app');
+            $table->dropColumn('is_enable_for_mobile_app');
+        });
+    }
+};
+                
